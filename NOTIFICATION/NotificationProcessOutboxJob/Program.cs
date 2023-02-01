9af799cb29b8ea49f9577141c0f60dfe0aaa0f3b
Yasemin Gerboğa <yasemingerboga@gmail.com>
2023-02-01 23:43:40 +0300

    create notification process outbox job (producer)

--- NOTIFICATION/NotificationProcessOutboxJob/Program.cs
+++ NOTIFICATION/NotificationProcessOutboxJob/Program.cs
@@ -1,9 +1,37 @@
-using NotificationProcessOutboxJob;
+using Application.Producers;
+using ApplicationNotification.Repository;
+using Microsoft.EntityFrameworkCore;
+using NotificationProcessOutboxJob.Jobs;
+using PersistanceNotification.Context;
+using PersistanceNotification.Repositories;
+using Quartz;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((hostContext, services) =>
     {
-        services.AddHostedService<Worker>();
+        services.AddQuartz(configurator =>
+        {
+            configurator.UseMicrosoftDependencyInjectionJobFactory();
+
+            JobKey jobKey = new("NotificationOutboxPublishJob");
+
+            configurator.AddJob<NotificationOutboxPublishJob>(options => options.WithIdentity(jobKey));
+
+            TriggerKey triggerKey = new("NotificationOutboxPublishJob");
+            configurator.AddTrigger(options => options.ForJob(jobKey)
+                        .WithIdentity(triggerKey)
+                        .StartAt(DateTime.UtcNow)
+                        .WithSimpleSchedule
+                        (
+                            builder => builder.WithIntervalInSeconds(5)
+                                              .RepeatForever()
+                        ));
+        });
+
+        services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+        services.AddScoped<IEventProducer, EventProducer>();
+        services.AddScoped<INotificationOutboxRepository, NotificationOutboxRepository>();
+        services.AddDbContext<NotificationDbContext>(options => options.UseSqlServer("Server=localhost,1466;Database=NotificationDb; TrustServerCertificate=True; User = sa; Password=password123*"));
     })
     .Build();
 
